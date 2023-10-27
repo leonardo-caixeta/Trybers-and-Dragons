0e81387130943f5407c925b7121eedc119b3f643
leonardo <leonardocaixeta07@gmail.com>
2023-10-27 01:08:45 -0300

    feat: requisitos 4

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,29 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  constructor(
+    private readonly _name: string,
+    private readonly _special: number = 0,
+    private readonly _cost: number = 0,
+  ) { }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
